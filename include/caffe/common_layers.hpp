81c73988be7b137e1c9081a760f1f9a2636b54cf
xiaolonw <dragon123@dragon123-M51AC.(none)>
2015-08-30 19:15:29 -0400

    reshape_block_layer

--- include/caffe/common_layers.hpp
+++ include/caffe/common_layers.hpp
@@ -453,9 +453,9 @@ class ReshapeBlockLayer : public Layer<Dtype> {
 
  protected:
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) {}
+      const vector<Blob<Dtype>*>& top);
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {}
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
   virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {}
   virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
