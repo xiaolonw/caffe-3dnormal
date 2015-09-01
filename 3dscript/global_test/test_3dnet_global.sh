c3bbb713c13225024275676645ab0aa1eb591fa2
Xiaolong Wang <xiaolonw@compute-1-1.local>
2015-09-01 15:16:55 -0400

    local_test

--- 3dscript/global_test/test_3dnet_global.sh
+++ 3dscript/global_test/test_3dnet_global.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh                                                                                                
 
+
 GLOG_logtostderr=1  /nfs/hn46/xiaolonw/3dnormal_joint_cnncode/caffe-3dnormal/build/tools/test_net_3dnormal_global.bin seg_test_2fc_3dnormal_layout.prototxt  /nfs/ladoga_no_backups/users/xiaolonw/3dnormal_release/global_model/3dnormal__iter_150000  /nfs/hn46/xiaolonw/cnncode/viewer/testLabels_single.txt    /nfs/ladoga_no_backups/users/xiaolonw/3dnormal_release/results/txts_global  /nfs/ladoga_no_backups/users/xiaolonw/3dnormal_release/global_model/layouttxt
 
 
