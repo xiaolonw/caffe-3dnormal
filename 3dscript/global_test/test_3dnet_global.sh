5c420577accffc5947395ddbf6a723ecd9992187
Xiaolong Wang <xiaolonw@yoda.graphics.cs.cmu.edu>
2015-09-01 00:56:56 -0400

    global

--- 3dscript/global_test/test_3dnet_global.sh
+++ 3dscript/global_test/test_3dnet_global.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh                                                                                                
 
-GLOG_logtostderr=1  /home/dragon123/3dnormal_joint_cnncode/caffe-3dnormal/build/tools/test_net_3dnormal_global.bin seg_test_2fc_3dnormal_layout.prototxt /home/dragon123/3dnormal_joint_cnncode/models/global_model/3dnormal__iter_150000  /home/dragon123/3dnormal_joint_cnncode/imgfile_global.txt /home/dragon123/3dnormal_joint_cnncode/slide_results/txts_global /home/dragon123/3dnormal_joint_cnncode/models/global_model/layouttxt
+GLOG_logtostderr=1  /nfs/hn46/xiaolonw/3dnormal_joint_cnncode/caffe-3dnormal/build/tools/test_net_3dnormal_global.bin seg_test_2fc_3dnormal_layout.prototxt  /nfs/ladoga_no_backups/users/xiaolonw/3dnormal_release/global_model/3dnormal__iter_150000  /nfs/hn46/xiaolonw/cnncode/viewer/testLabels_single.txt    /nfs/ladoga_no_backups/users/xiaolonw/3dnormal_release/results/txts_global  /nfs/ladoga_no_backups/users/xiaolonw/3dnormal_release/global_model/layouttxt
+
 
 
 
